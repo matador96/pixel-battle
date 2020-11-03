40961c67cf4dbc3be9f117ee5bffb82596c49811
Altun Ismailov <atubrah@mail.ru>
2020-11-04 02:58:08 +0700

    any stuff

--- src/App.js
+++ src/App.js
@@ -3,25 +3,78 @@ import "./style.css";
 import colors from "./consts/colors";
 import users from "./consts/users";
 import Canvas from "./components/Canvas";
+import StyledReactPanZoom from "@ajainarayanan/react-pan-zoom";
 
-const width = 800;
-const height = 500;
+// const width = 800;
+// const height = 500;
 
-const draw = (ctx, frameCount) => {
-  const positionY = 5;
-  const positionX = 5;
-  const pixelWith = 1;
-  const pixelHeight = 1;
+// const draw = (ctx, frameCount) => {
+//   const positionY = 5;
+//   const positionX = 5;
+//   const pixelWith = 1;
+//   const pixelHeight = 1;
 
-    // for (let indexW = 0; indexW < width; indexW++) {
-    //   for (let indexY = 0; indexY < height; indexY++) {
-    //     ctx.fillRect(indexW, indexY, pixelWith, pixelHeight);
-    //     ctx.fillStyle = "#f1f1f1";
-    //   }
-    // }
-};
+//   // for (let indexW = 0; indexW < width; indexW++) {
+//   //   for (let indexY = 0; indexY < height; indexY++) {
+//   //     ctx.fillRect(indexW, indexY, pixelWith, pixelHeight);
+//   //     ctx.fillStyle = "#f1f1f1";
+//   //   }
+//   // }
+// };
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // Не вызывайте здесь this.setState()!
+    this.state = { dx: 0, dy: 0, zoom: 1 };
+  }
+
+  componentDidMount() {
+    // window.addEventListener(
+    //   "wheel",
+    //   (event) => {
+    //     var oldPos = window.scrollY;
+    //     const { ctrlKey } = event;
+    //     var newPos = window.scrollY;
+    //     if (newPos > oldPos) {
+    //       this.zoomOut();
+    //     } else if (newPos < oldPos) {
+    //       this.zoomIn();
+    //       // } else {
+    //       //   console.log("same");
+    //       // }
+    //     }
+    //     // console.log(event);
+    //     // if (ctrlKey) {
+    //     //   console.log(ctrlKey);
+    //     //   // eslint-disable-next-line no-unused-expressions
+    //     //   event.preventDefault;
+    //     //   return;
+    //     // }
+    //   },
+    //   { passive: false }
+    // );
+  }
+
+  zoomIn = () => {
+    this.setState({
+      zoom: this.state.zoom + 0.2,
+    });
+  };
+
+  zoomOut = () => {
+    this.setState({
+      zoom: this.state.zoom - 0.2,
+    });
+  };
+
+  onPan = (dx: number, dy: number) => {
+    this.setState({
+      dx,
+      dy,
+    });
+  };
+
   render() {
     return (
       <div className="App">
@@ -57,8 +110,20 @@ export default class App extends React.Component {
             </div>
           </div>
         </div>
+        <div>
+          <button onClick={this.zoomOut}>-</button>
+          <input type="range" id="volume" name="volume" min="0" max="11"/>
+          <button onClick={this.zoomIn}>+</button>
+        </div>
         <div className="canvas-block">
-          <Canvas draw={draw} />
+        *<StyledReactPanZoom
+            zoom={this.state.zoom}
+            pandx={this.state.dx}
+            pandy={this.state.dy}
+            onPan={this.onPan}
+          > 
+            <Canvas zoomIn={this.state.zoomIn} zoomOut={this.state.zoomOut}/>
+         </StyledReactPanZoom>
         </div>
       </div>
     );
--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,23 +1,44 @@
 import React from "react";
+import colors from "./../consts/colors";
+
 export default class Canvas extends React.Component {
+  constructor(props) {
+    super(props);
+   
+  }
   componentDidMount() {
     this.updateCanvas();
   }
   updateCanvas() {
+
+    //https://jsfiddle.net/akinuri/cbx7pmra/ Зум здесь
     const ctx = this.refs.canvas.getContext("2d");
+    const max = 50;
+    ctx.scale(max,max);
     const positionY = 5;
     const positionX = 5;
     const pixelWith = 1;
     const pixelHeight = 1;
-    const width = 800;
-    const height = 500;
+    const width = 3000;
+    const height = 3000;
 
     for (let indexW = 0; indexW < width; indexW++) {
       for (let indexY = 0; indexY < height; indexY++) {
         ctx.fillRect(indexW, indexY, pixelWith, pixelHeight);
-        ctx.fillStyle = "#f1f1f1";
+        ctx.fillStyle = this.randomColor();
       }
     }
+  }
+
+  componentDidUpdate(prevProps){
+    if(prevProps.zoomIn !== this.props.zoomIn){
+      this.updateCanvas();
+    }
+  }
+
+  randomColor() {
+    let key = Math.floor(Math.random() * Math.floor(20));
+    return colors[key].hex;
   }
   render() {
     return <canvas ref="canvas" width={800} height={502} class="drawer" />;
